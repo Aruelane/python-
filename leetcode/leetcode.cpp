7c9e93ace1c78dee9aa5db5136e389d085a20711
zack <8156341+yu-gui-wan@user.noreply.gitee.com>
2026-02-07 22:02:25 +0800

    2026.2.7

--- leetcode/leetcode.cpp
+++ leetcode/leetcode.cpp
@@ -243,7 +243,48 @@ struct ListNode* removeElements(struct ListNode* head, int val) {
     }
     return head;
 }
+//删除排序链表中的重复元素 II
+struct ListNode {
+    int val;
+    struct ListNode *next;
+};
 
+struct ListNode* deleteDuplicates(struct ListNode* head) {
+    if (!head)
+        return head;
+    struct ListNode* p = head, * q;
+    int* arr = (int*)malloc(sizeof(int) * 201);
+    int i;
+    for (i = 0; i < 201; i++) {
+        arr[i] = 0;
+    }
+    while (p) {
+        if (arr[p->val + 100] == 0) {
+            arr[p->val + 100] = 1;
+        }
+        else if (arr[p->val + 100] == 1) {
+            arr[p->val + 100] = 2;
+        }
+        p = p->next;
+    }
+    p = head, q = head;
+    int flag = 0;
+    for (i = 0; i < 201; i++) {
+        if (arr[i] == 1) {
+            p->val = i - 100;
+            p = p->next;
+            flag = 1;
+        }
+    }
+    if (flag == 0)return NULL;
+    while (q && q->next != p) {
+        q = q->next;
+    }
+    if (q) {
+        q->next = NULL;
+    }
+    return head;
+}
 
 
 int main(void) {
